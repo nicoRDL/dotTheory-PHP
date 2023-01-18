5c8f4a8691b88781dc93437b2c6389cbb1db7855
NicolaiLee <nicolai.rvyn.lee@codeandtheory.com>
2023-01-18 15:04:21 +0800

    code solution for exam 1

---
+++ php-103/exam1/index.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+<?php 
+  include 'function.php';
+?>
+<form action="index.php" method="post">
+Enter a number: <input type="number" name="inputNumber"><br>
+<input type="submit">
+</form>
+</body>
+</html>
